a579cd0a59c4ca7132d45b0f7054c05cabcc3d5a
syaheer-altaf <altafsyaheer@gmail.com>
2025-05-19 01:49:29 +0800

    modified: 20250501_create_all.rb

--- db/seeds/20250501_create_all.rb
+++ db/seeds/20250501_create_all.rb
@@ -4,8 +4,7 @@ Sequel.seed(:development) do
   def run
     puts 'Seeding accounts, flocks, birds'
     create_accounts
-    create_flocks_and_initial_birds
-    add_extra_birds
+    create_flocks
   end
 end
 
@@ -20,13 +19,11 @@ BIRD_INFO = YAML.load_file("#{DIR}/bird_seeds.yml")
 
 def create_accounts
   ACCOUNTS_INFO.each do |account_info|
-    Flocks::Account.find_or_create(username: account_info['username']) do |acc|
-      acc.set_fields(account_info, %w[email password])
-    end
+    Flocks::Account.create(username: account_info['username'], password: account_info['password'])
   end
 end
 
-def create_flocks_and_initial_birds
+def create_flocks
   ACCOUNTS_INFO.zip(FLOCK_INFO, BIRD_INFO).each do |account_data, flock_data, bird_data|
     username = account_data['username']
 
@@ -40,23 +37,4 @@ def create_flocks_and_initial_birds
 
     Flocks::CreateFlock.call(username: username, flock_data: full_flock_data)
   end
-end
-
-def add_extra_birds
-  accounts = Flocks::Account.all
-  flocks = Flocks::Flock.all
-
-  # Add second bird from account[0] to another flock if it exists
-  account = accounts.first
-  flock = flocks.last
-  return if accounts.size < 2 || flocks.size < 2
-
-  extra_bird_data = {
-    latitude_secure: SecureDB.encrypt('24.800000'),
-    longitude_secure: SecureDB.encrypt('120.990000'),
-    message_secure: SecureDB.encrypt('Bonus bird!'),
-    account_id: account.id
-  }
-
-  Flocks::AddBirdToFlock.call(flock_id: flock.id, bird_data: extra_bird_data)
 end
